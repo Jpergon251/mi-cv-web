f66194534bd1ee4090a50e40be235261e9d36292
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2025-07-29 13:58:41 +0200

    Añadida linea Base de nuevo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES'
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGitHubPages ? '/mi-cv-web/' : '/', // usa '/' para Netlify
   plugins: [vue()],
 })
