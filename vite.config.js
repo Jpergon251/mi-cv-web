08ae2e8095f1c64d323c3505312be72259bd9b8e
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2025-07-29 14:18:42 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES'
 // https://vite.dev/config/
 export default defineConfig({
-  base: isGitHubPages ? '/mi-cv-web/' : '/', // usa '/' para Netlify
+  base:  '/mi-cv-web/', // usa '/' para Netlify
   plugins: [vue()],
 })
