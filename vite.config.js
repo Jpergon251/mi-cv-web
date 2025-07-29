611a1d489bc6dea252cbf833ae4a0c7c7969a6d4
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2025-07-29 13:44:15 +0200

    Eliminada linea Base de la configuracion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Jpergon251/mi-cv-web', // muy importante
   plugins: [vue()],
 })
