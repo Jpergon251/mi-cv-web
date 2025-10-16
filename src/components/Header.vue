df021f554fa59d62ab0618a0d18b409c41b6035b
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2025-10-16 12:47:02 +0200

    Nuevo PDF mejorado

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -64,53 +64,10 @@ import { jsPDF } from "jspdf";
 
 // Método para descargar el CV
 const descargarCV = () => {
-  const doc = new jsPDF();
-
-  // === CABECERA ===
-  doc.setFontSize(22);
-  doc.text("José Antonio Pérez González", 20, 30);
-
-  // Foto de perfil (a la derecha)
-  const imgUrl = "https://avatars.githubusercontent.com/u/114741595?v=4";
-  const img = new Image();
-  img.crossOrigin = "Anonymous";
-  img.src = imgUrl;
-
-  img.onload = () => {
-    doc.addImage(img, "PNG", 160, 15, 40, 40); // derecha arriba
-
-    // === TITULACIONES ===
-    doc.setFontSize(14);
-    doc.text("Técnico Superior en Desarrollo de Aplicaciones Web", 20, 45);
-    doc.text("Especialista en Desarrollo de Videojuegos y Realidad Virtual", 20, 55);
-
-    // === DESCRIPCIÓN PERSONAL ===
-    doc.setFontSize(12);
-    doc.text("Soy un desarrollador apasionado por la tecnología, la innovación y el aprendizaje constante.", 20, 70, { maxWidth: 170 });
-
-    // === EXPERIENCIA / LO QUE HAGO ===
-    doc.setFontSize(12);
-    doc.text("Actualmente desarrollo videojuegos de manera independiente, centrado en la programación y el diseño de mecánicas. Busco oportunidades para crecer profesionalmente y aplicar mis conocimientos en proyectos reales.", 20, 85, { maxWidth: 170 });
-
-    doc.setFontSize(12);
-    doc.text("Tengo experiencia en backend y frontend, he practicado con lenguajes como Java, JavaScript, Python, PHP, TypeScript, Kotlin y C#. He utilizado frameworks como Spring Boot, Laravel, Vue y React.", 20, 105, { maxWidth: 170 });
-
-    // === ENLACES / CONTACTO ===
-    doc.setFontSize(12);
-    doc.text("Para ver la informacion detallada tengo mi Curriculum Web", 20, 125);
-    //color Azul subrayado
-    doc.setTextColor(0, 102, 204);
-    doc.setFont("Helvetica", "bold");
-    doc.textWithLink("Curriculum Web", 20, 135, { url: "https://joseantonioperezcurriculum.netlify.app/" });
-
-    doc.setFont("Helvetica", "normal");
-    doc.setTextColor(0, 0, 0); // Volver al color negro
-    doc.text("+34 601 251 537", 20, 145);
-    doc.textWithLink("joseperzant00@gmail.com", 20, 155, { url: "mailto:joseperzant00@gmail.com" });
-
-    // Guardar PDF
-    doc.save("CV_Jose_Antonio_Perez.pdf");
-  };
+  const link = document.createElement("a");
+  link.href = "/Files/CV_Jose_Antonio_Perez_Gonzalez.pdf"; // ruta dentro de /public
+  link.download = "CV_Jose_Antonio_Perez_Gonzalez.pdf";
+  link.click();
 };
 useTilt('.tilt')
 </script>
