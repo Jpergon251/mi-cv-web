9761cceed7ea7491927bba2d4eb25230e7c938a3
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2026-05-27 19:15:37 +0200

    nueva correccion de los emails
    Sintaxis simplificada

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -32,7 +32,7 @@
     <nav class="contact-menu-nav hidden-nav" v-if="isContactMenuOpen">
         <a href="https://www.linkedin.com/in/jos%C3%A9-antonio-p%C3%A9rez-gonz%C3%A1lez-977999330/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i> LinkedIn</a>
         <a href="https://github.com/Jpergon251" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i> GitHub</a>
-        <a :href="'mailto:'+$t('home.email')" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i>Gmail</a>
+        <a href="joseperzant00@gmail.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i>Gmail</a>
         <a :href="`https://wa.me/${$t('home.whatsapp')}`" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp"></i>WhatsApp</a>
         <!-- <a href="https://friki-snake.itch.io/" target="_blank" rel="noopener noreferrer"><i class="fab fa-itch-io"></i>Itch.io</a> -->
     </nav>   
@@ -41,7 +41,7 @@
       
         <a href="https://www.linkedin.com/in/jos%C3%A9-antonio-p%C3%A9rez-gonz%C3%A1lez-977999330/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
         <a href="https://github.com/Jpergon251" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-        <a :href="'mailto:'+$t('home.email')" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i></a>
+        <a href="joseperzant00@gmail.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i></a>
         <a :href="`https://wa.me/${$t('home.whatsapp')}`" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp"></i></a>
         <!-- <a href="https://friki-snake.itch.io/" target="_blank" rel="noopener noreferrer"><i class="fab fa-itch-io"></i></a> -->
     </nav>
