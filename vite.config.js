589aa199f2f6ddb0de9c3694c39ba56c270818a1
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2026-08-17 13:31:58 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES'
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/mi-cv-web/',
 })
